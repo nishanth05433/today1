64bdd19644c5e6f132ec75ca7fbbbabe531bcb3b
nishanth05433 <suma9453@gmail.com>
2026-06-22 12:45:18 +0530

    first commit

---
+++ src/main/java/com/example/smple/App.java
@@ -0,0 +1,13 @@
+package com.example.smple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
